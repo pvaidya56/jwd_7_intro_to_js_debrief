e8deee9744a291a50786144a4d0a938e5d053f77
Andre Romero <romeroandre801@outlook.com>
2022-11-07 20:48:14 -0700

    fixed error in getrandomnumber function

--- js/app.js
+++ js/app.js
@@ -66,8 +66,9 @@ switch(i) {
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Write your code below this line:
+
 function getRandomNum(min, max) {
-    console.log(Math.floor(Math.random() * max - min + 1)) + min;
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
 getRandomNum(50, 100);
